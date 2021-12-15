5197f0d3757f245f083e3d9682d4e159378234ac
Prenkov <76231890+Prenkov@users.noreply.github.com>
2021-12-15 21:38:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+// 1.
+var number1 = prompt("Enter the first number");
+var number2 = prompt("Enter the second number");
+var multi = number1 * number2;
+var divi = number1 / number2;
+console.log("a * b = " + multi + "\n" + "a / b = " + divi); 
+alert("a * b = " + multi + "\n" + "a / b = " + divi);
+
+// 2.
+var firstSide = prompt("Enter the first side of the Rectangle");
+var secondSide = prompt("Enter the second side of the Rectangle");
+var p = firstSide * 2 + secondSide * 2;
+console.log("The perimeter of the Rectangle is: " + p);
+alert("The perimeter of the Rectangle is: " + p);
+
+// 3.
+  var celsius = prompt("Enter the temperature in celsius");
+  var fahrenheit = celsius * 9 / 5 + 32;
+  console.log("The temperature in fahrenheit is: " + fahrenheit);
+  alert("The temperature in fahrenheit is: " + fahrenheit)
+
+// 4.
+var Chikens = prompt("Enter the number of Chikens");
+var Cows = prompt("Enter the number of Cows");
+var Pigs = prompt("Enter the number of Pigs");
+Chikens *= 2;
+Cows *= 4;
+Pigs *= 4;
+total = Chikens + Cows + Pigs;
+console.log("The number of legs of all the Chikens is: " + Chikens);
+console.log("The number of legs of all the Cows is: " + Cows);
+console.log("The number of legs of all the Pigs is: " + Pigs);
+alert("The number of legs of all the Chikens is: " + Chikens + "\n" + "The number of legs of all the Chikens is: " + Cows + "\n" + "The number of legs of all the Chikens is: " + Pigs);
+console.log("The total number of legs of all the animals is: " + total);
+alert("The total number of legs of all the animals is: " + total);
+
